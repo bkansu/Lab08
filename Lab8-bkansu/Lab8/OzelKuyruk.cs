d999a9c352553a123d83b704283600e63c6b8e71
Batuhan Kansu <47796417+bkansu@users.noreply.github.com>
2019-04-25 14:36:28 +0300

    Add files via upload

---
+++ Lab8-bkansu/Lab8/OzelKuyruk.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class OzelKuyruk
+    {
+        ArrayList sayilar=new ArrayList();
+        public static int ElemanSayisi=0;        
+        public void Enqueue(int YeniEleman)
+        {
+            bool flag = false;
+            
+            if (ElemanSayisi != 0)
+            {
+
+            
+            for (int i = 0; i < sayilar.Count; i++)
+            {
+                if(Convert.ToInt32(sayilar[i]) % 2 == 0)
+                {
+                        sayilar.Insert(i+1, YeniEleman);
+                        flag = true;
+                }
+            }
+                if (flag == false)
+                {
+                    sayilar.Insert(sayilar.Count, YeniEleman);
+                }
+            }
+            else
+            {
+                sayilar.Insert(0, YeniEleman);
+            }
+            for (int i = 0; i < sayilar.Count; i++)
+            {
+                Console.Write(sayilar[i] + " ");
+            }
+            Console.WriteLine("");
+            //implemente edilecek
+            ElemanSayisi++;
+        }
+
+        public void Dequeue()
+        {
+            //implemente edilecek
+            if (ElemanSayisi == 0)
+            {
+                Console.WriteLine("Dizi boş");
+            }
+            else
+            {
+                sayilar.RemoveAt(sayilar.Count / 2);
+                for (int i = 0; i < sayilar.Count; i++)
+                {
+                    Console.Write(sayilar[i] + " ");
+                }
+                Console.WriteLine("");
+            }
+            //OzelKuyruk boş ise istisna fırlatacak
+        }
+
+        
+    }
+}
---
+++ Lab8-bkansu/Lab8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***X***");
+            OzelKuyruk x = new OzelKuyruk();
+            
+            x.Enqueue(1);
+            x.Enqueue(5);
+            x.Enqueue(24);
+            x.Enqueue(7);
+            x.Enqueue(9);                   
+            x.Enqueue(15);
+            x.Enqueue(17);
+            x.Enqueue(7);
+            x.Dequeue();
+
+            Queue y = new Queue();
+            y.Enqueue(1);
+            y.Enqueue(5);
+            y.Enqueue(7);
+            y.Enqueue(9);
+            y.Enqueue(16);
+            y.Enqueue(24);
+            y.Enqueue(7);
+            y.Dequeue();
+
+            Console.WriteLine("\n***Y***");
+            for (int i = 0; i < y.Count; i++)
+            {
+                Console.Write((y.ToArray())[i]+" ");
+            }
+            Console.ReadLine();
+
+            //Çeşitli ekleme ve çıkarma yaptıktan sonra kuyrukların görüntüsünü adım adım ekranda gösteriniz
+            //yukarıdaki eklemeler
+        }
+    }
+}
