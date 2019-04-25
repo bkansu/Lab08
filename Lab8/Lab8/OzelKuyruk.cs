ed8fb078af1142f69c21aac06bf3f0517cdef832
baristezel <baris.tezel@deu.edu.tr>
2019-04-25 11:14:55 +0300

    Add files via upload

---
+++ Lab8/Lab8/OzelKuyruk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class OzelKuyruk
+    {
+        ArrayList sayilar;
+        public static int ElemanSayisi;
+
+        public void Enqueue(int YeniEleman)
+        {
+            //implemente edilecek
+        }
+
+        public void Dequeue()
+        {
+            //implemente edilecek
+
+            //OzelKuyruk boş ise istisna fırlatacak
+        }
+
+        
+    }
+}
---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OzelKuyruk x = new OzelKuyruk();
+            x.Enqueue(1);
+            x.Enqueue(5);
+            x.Enqueue(7);
+            x.Enqueue(9);
+            x.Enqueue(16);
+            x.Enqueue(24);
+            x.Enqueue(7);
+
+            Queue y = new Queue();
+            y.Enqueue(1);
+            y.Enqueue(5);
+            y.Enqueue(7);
+            y.Enqueue(9);
+            y.Enqueue(16);
+            y.Enqueue(24);
+            y.Enqueue(7);
+
+            //Çeşitli ekleme ve çıkarma yaptıktan sonra kuyrukların görüntüsünü adım adım ekranda gösteriniz
+            //yukarıdaki eklemeler
+        }
+    }
+}
